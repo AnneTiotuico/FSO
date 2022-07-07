aac32cb2973e0a31d9ad781c9431c6d0edd5dfae
Anne Tiotuico <amtiotuico@gmail.com>
2022-07-07 08:56:24 -0700

    modified App to use Course component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,53 +1,57 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
-    </div>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const courses = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
   ]
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => {
+        return <Course course={course} />
+      })}
     </div>
   )
 }
