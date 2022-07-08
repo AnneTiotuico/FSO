194ed824e8489361556acdfa30c2346cf41d2017
Anne Tiotuico <amtiotuico@gmail.com>
2022-07-08 08:31:03 -0700

    ex 2.11 refactored and stored initial state in db.json file

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then(response => response.json())
+      .then(persons => setPersons(persons))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,11 +34,10 @@ const App = () => {
     setShowAll(false)
   }
 
-  const displayPersons = () => {  
-    return showAll
-    ? persons
-    : persons.filter(({ name }) => name.toLowerCase().includes(newSearch.toLowerCase()))
-  }
+  const filteredPersons = showAll
+                          ? persons
+                          : persons.filter(({ name }) => 
+                            name.toLowerCase().includes(newSearch.toLowerCase()))
 
   return (
     <div>
@@ -45,7 +45,7 @@ const App = () => {
       <Search newSearch={newSearch} handleNewSearch={handleNewSearch}/>
       <PersonForm addPerson={addPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
       <h2>Numbers</h2>
-      <Persons persons={displayPersons()} />
+      <Persons persons={filteredPersons} />
     </div>
   )
 }
